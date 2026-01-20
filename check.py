cec99c4b57f3f130b3396e5fb137ac234197700b
risolat-dev <risolat0990@icloud.com>
2026-01-20 07:41:21 +0500

    Initial commit: Sushi Shop Bot

---
+++ check.py
@@ -0,0 +1,23 @@
+# a=12
+# b=34
+# c=a+b
+# print(c)
+#
+# x, y, z = 1, 4, 5
+# print(x)
+# print(y)
+# print(z)
+#
+# x = 5
+# x = complex(x)
+# print(x)
+#
+# x = 'Welcome'
+# print(x[3])
+#
+# txt = "Hello World"
+# x = txt[0]
+
+import random
+
+print(random.randrange(1, 10))
