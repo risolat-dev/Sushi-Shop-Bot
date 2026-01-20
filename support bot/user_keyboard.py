c137d50618d8c90fa75586652ecffc0d38e9a3a6
risolat-dev <risolat0990@icloud.com>
2026-01-20 09:57:12 +0500

    Remove unnecessary files: reminders, support bot, check.py

--- support bot/user_keyboard.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def next_lesson_keyboard():
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="Keyingi dars ➡️", callback_data="next_lesson")]
-        ]
-    )
-    return keyboard
